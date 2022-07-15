154c1ddb3fffd8a78a7ee316634bc81daeb1c3e5
kard-py <caiodetz654@gmail.com>
2022-07-15 16:44:41 -0400

    next.config

--- next.config.js
+++ next.config.js
@@ -4,12 +4,19 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, options) => {
+    config.externals = {
+      sharp: "commonjs sharp",
+    };
     config.resolve.fallback = {
       fs: false,
       dns: false,
       tls: false,
       net: false,
       child_process: false,
+      bson_ext: false,
+      kerberos: false,
+      snappy: false,
+      aws4: false,
     };
     config.experiments = {
       topLevelAwait: true,
