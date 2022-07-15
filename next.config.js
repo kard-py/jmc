d3c7b3a074c0af462807e53af6e2fe7e66b2d135
kard-py <caiodetz654@gmail.com>
2022-07-15 16:28:10 -0400

    next.config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, options) => {
-    config.resolve.fallback = { fs: false, dns: false, tls: false, net: false };
+    config.resolve.fallback = {
+      fs: false,
+      dns: false,
+      tls: false,
+      net: false,
+      child_process: false,
+    };
     config.experiments = {
       topLevelAwait: true,
       layers: true,
