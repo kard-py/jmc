b4b0e361a83d7e48e13cc2c1bcf6f91a1b1c561c
kard-py <caiodetz654@gmail.com>
2022-07-18 13:54:08 -0400

    teste Login

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -14,13 +14,22 @@ const Login = async (req, res) => {
   const { db } = await connect();
 
   let user = await db.collection("users").findOne(data);
-  res.status(200).json({
-    error: "null",
-    token_jwt: jwt.sign(user.data, process.env.SECRET_KEY, {
-      expiresIn: 60 * 45,
-    }),
-    userData: user.data,
-  });
+
+  if (user !== null) {
+    res.status(200).json({
+      error: "null",
+      token_jwt: jwt.sign(user.data, process.env.SECRET_KEY, {
+        expiresIn: 60 * 45,
+      }),
+      userData: user.data,
+    });
+    res.end();
+  }
+
+  res
+    .status(401)
+    .json({ error: "LOGIN NÃO AUTORIZADO", token_jwt: "NOT_AUTH" });
+
   res.end();
 };
 export default Login;
