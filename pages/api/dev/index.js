fa3337e8d41a74697051039d44417e8c86b7a34a
kard-py <caiodetz654@gmail.com>
2022-08-02 16:57:08 -0400

    update dev api

--- pages/api/dev/index.js
+++ pages/api/dev/index.js
@@ -3,9 +3,13 @@ import jwt from "jsonwebtoken";
 const Login = async (req, res) => {
   res.status(200).json({
     error: "null",
-    token_jwt: jwt.sign(users[i].data, process.env.SECRET_KEY, {
-      expiresIn: 60 * 45,
-    }),
+    token_jwt: jwt.sign(
+      { erro: "dá errro Agora filha da puta" },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: 60 * 45,
+      }
+    ),
     userData: {},
   });
   res.end();
